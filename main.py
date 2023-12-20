753b75a4e4cbba8c25eff471d4651d14cc99c874
ImWombat <139051235+ImWombat@users.noreply.github.com>
2023-12-21 00:07:57 +0300

    id leaks

--- main.py
+++ main.py
@@ -113,13 +113,18 @@ async def weight_handler(message: types.Message):
     else:
         await message.reply("У вас нет хряка. Начните с команды /start.")
 
-
+def replace_name(name, user_id):
+    if "\n" in name or len(name) > 50:
+        return 'Я уебан с длинным ником, мой ID:' + str(user_id)
+    else:
+        return name
+        
 @dp.message_handler(commands=["top"])
 async def top_handler(message: types.Message):
     sorted_pigs = sorted(pigs.values(), key=lambda pig: pig.weight, reverse=True)
     top_message = "Топ 20 хряков по весу:\n\n"
     for index, pig in enumerate(sorted_pigs[:20], start=1):
-        top_message += f"{index}. {'Я уебан с длинным ником, мой ID:' + str(message.from_id) if len(pig.name) > 50 else pig.name if pig.name else 'Без имени'} - {pig.weight} кг\n"
+        top_message += f"{index}. {replace_name(pig.name, message.from_id)} - {pig.weight} кг\n"
     await message.reply(top_message)
 
 
